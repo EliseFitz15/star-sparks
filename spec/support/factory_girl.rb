9f2c4c7be4fc0d0e7f519d861e36cfb0f8cc32cb
Elise Fitzgerald <elise.fitzgerald15@gmail.com>
2016-01-03 13:08:49 -0500

    add products index and show

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -7,4 +7,9 @@ FactoryGirl.define do
     password_confirmation 'password'
   end
 
+  factory :product do
+    sequence(:name) {|n| "Taurus#{n}" }
+    description "Part of the astrology collection"
+    price "20.00"
+  end
 end
